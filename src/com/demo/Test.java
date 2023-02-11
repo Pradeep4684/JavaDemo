888e8fe3d1771d87fba5c2a7e5d419840cbf9ba8
Pradeep4684 <pradeep.andhale97@gmail.com>
2023-02-11 19:42:50 +0530

    HashMap demo code is added

--- src/com/demo/Test.java
+++ src/com/demo/Test.java
@@ -1,18 +1,21 @@
 package com.demo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Test {
 
 	public static void main(String[] args) {
 		
-		ArrayList al = new ArrayList();
-		
-		al.add("Pradeep");
-		al.add("Ram");
-		al.add("Shyam");
-		
-		al.stream().forEach(x->System.out.println(x));
+	HashMap<String,String> hashMap= new HashMap<String,String>();
+	
+	hashMap.put("Ram", "20");
+	hashMap.put("Shyam", "20");
+	hashMap.put("shiva", "15");
+	
+	hashMap.forEach((x,y)->System.out.println(x+ " " + y));
+	
 	}
 }
