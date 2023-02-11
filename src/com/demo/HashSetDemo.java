afbd2dc8901d62a1ced50a0cbdfded5e143836fb
Pradeep4684 <pradeep.andhale97@gmail.com>
2023-02-11 20:21:32 +0530

    HashSetDemo added

---
+++ src/com/demo/HashSetDemo.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+		
+		HashSet<Integer> hashSet= new HashSet<Integer>();
+		
+		hashSet.add(100);
+		hashSet.add(50);
+		hashSet.add(75);
+		
+		System.out.println(hashSet);
+	}
+	
+	
+}
