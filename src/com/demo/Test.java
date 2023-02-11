72eb1cc9a3b367dd8308de703c5af5ba6994b087
Pradeep4684 <pradeep.andhale97@gmail.com>
2023-02-11 16:06:52 +0530

    first commit : added arraylist demo

---
+++ src/com/demo/Test.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		ArrayList al = new ArrayList();
+		
+		al.add("Pradeep");
+		al.add("Ram");
+		al.add("Shyam");
+		
+		System.out.println(al);
+	}
+}
